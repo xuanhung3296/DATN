b1aa6c71e0d2bafcade369314c89e775eef19079
bacnl <bacnl@smartosc.com>
2019-06-28 19:00:41 +0700

    booking

--- TopTravel/Controllers/BookingController.cs
+++ TopTravel/Controllers/BookingController.cs
@@ -8,10 +8,23 @@ namespace TopTravel.Controllers
 {
     public class BookingController : Controller
     {
+        private BookingEntities db = new BookingEntities();
         // GET: Booking
-        public ActionResult Index()
+        public ActionResult Index(int id)
         {
-            return View();
+            var tour = db.Tours.FirstOrDefault(x => x.TourID == id);
+            var nguoiLonPrice = tour.ListedPrice;
+            var treEmPrice = tour.ListedPrice/2;
+            var treNhoPrice = tour.ListedPrice/3;
+            var emBePrice = tour.ListedPrice/4;
+            var phuThu = 100000;
+
+            ViewBag.AdultPrice = nguoiLonPrice;
+            ViewBag.ChildPrice = treEmPrice;
+            ViewBag.BabyPrice = treNhoPrice;
+            ViewBag.BabePrice = emBePrice;
+            ViewBag.Bonus = phuThu;
+            return View(tour);
         }
     }
 }
