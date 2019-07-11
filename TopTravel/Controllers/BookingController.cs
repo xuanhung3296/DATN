50c2051e3bc902ecb2c3adb0e37be39a9b99302f
bacnl <bacnl@smartosc.com>
2019-07-11 18:22:23 +0700

    update Controllers/BookingController.cs

--- TopTravel/Controllers/BookingController.cs
+++ TopTravel/Controllers/BookingController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TopTravel.Models;
 
 namespace TopTravel.Controllers
 {
@@ -50,39 +51,18 @@ namespace TopTravel.Controllers
                 touristList.Add(new Tourist());
             }
 
-
+            FormSubmitViewModel form = new FormSubmitViewModel();
+            form.User = user;
+            form.ListTourist = touristList;
             ViewBag.AdultPrice = tour.ListedPrice;
             ViewBag.ChildrenPrice = tour.ListedPrice/2;         
-            return View("Form",touristList);
+            return View("Form", form);
         }
 
-        public ActionResult AddUser(string name, string phone, string address, string birthday, string email, string numberAdult, string numberChildren, string numberBaby, string numberBabe, string guests, string id)
+        public ActionResult BookTour(User user, List<Tourist> list, string  checkRule , string payMethod, string total )
         {
-            User user = new User();
-            user.Name = name;
-            user.Phone = phone;
-            user.Address = address;
-            user.Email = email;
-            ViewBag.NumberAdult = numberAdult;
-            ViewBag.NumberChildren = numberChildren;
-            ViewBag.NumberBaby = numberBaby;
-            ViewBag.NumberBabe = numberBabe;
-            ViewBag.Guest = guests;
-            var ID = Convert.ToInt32(id);
-            var tour = db.Tours.FirstOrDefault(x => x.TourID == ID);
 
-
-            var touristList = new List<Tourist>();
-            // Another one is using for loop
-            for (int i = 0; i < Convert.ToInt32(guests); i++)
-            {
-                touristList.Add(new Tourist());
-            }
-
-
-            ViewBag.AdultPrice = tour.ListedPrice;
-            ViewBag.ChildrenPrice = tour.ListedPrice / 2;
-            return View("Form", touristList);
+            return View("Error");
         }
 
 
